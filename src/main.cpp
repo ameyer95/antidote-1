265993f5629a5de98c8eacefd38007dd30db385f
sedrews <sedrews@wisc.edu>
2019-06-14 15:48:20 -0500

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Experiment.h"
 #include <iostream>
 #include <utility>
+#include <cstdint>
 using namespace std;
 
 
@@ -17,6 +18,27 @@ void test_build_ast(int depth) {
 void test_build_asts() {
     for(int i = 0; i < 3; i++) {
         test_build_ast(i);
+    }
+}
+
+void print_digit(uint8_t *start) {
+    for(int row = 0; row < 28; row++) {
+        for(int col = 0; col < 28; col++) {
+            uint8_t pixel = *(start + 28*row + col);
+            cout << (pixel < 128 ? " " : "0");
+        }
+        cout << endl;
+    }
+}
+
+void test_load_MNIST() {
+    pair<LabelFile, ImageFile> training, test;
+    training = MNIST_readTrainingSet("data/");
+    test = MNIST_readTestSet("data/");
+    for(int i = 0; i < 10; i++) {
+        cout << "This is supposedly a " << (int)training.first.labels[i] << endl;
+        print_digit(training.second.pixels + i*28*28*sizeof(uint8_t));
+        cout << "==================================================" << endl << endl;
     }
 }
 
