a26ff560a8206bd51f58b3dfa7fff0d9ce45b637
Samuel Drews <sedrews@wisc.edu>
2019-10-04 01:12:36 -0500

    unbreaking meager tests

--- test/src/test_data_common.cpp
+++ test/src/test_data_common.cpp
@@ -11,18 +11,18 @@ TEMPLATE_TEST_CASE("DataReferences<T> basic operations", "", int, vector<int>) {
     REQUIRE(data_references.size() == raw_data.size());
 
     SECTION("DataReferences<T>::operator [] returns correct objects") {
-        for(int i = 0; i < data_references.size(); i++) {
+        for(unsigned int i = 0; i < data_references.size(); i++) {
             REQUIRE(&(data_references[i]) == &(raw_data[i]));
         }
     }
 
     SECTION("DataReferences<T>::remove affects only the references and is correct") {
-        const int REMOVE_INDEX = 1;
+        const unsigned int REMOVE_INDEX = 1;
         data_references.remove(REMOVE_INDEX);
         REQUIRE(data_references.size() == SIZE - 1);
         REQUIRE(raw_data.size() == SIZE);
-        for(int i = 0; i < data_references.size(); i++) {
-            int raw_index = (i >= REMOVE_INDEX ? i + 1 : i);
+        for(unsigned int i = 0; i < data_references.size(); i++) {
+            unsigned int raw_index = (i >= REMOVE_INDEX ? i + 1 : i);
             REQUIRE(&(data_references[i]) == &(raw_data[raw_index]));
         }
     }
--- test/src/test_information_math.cpp
+++ test/src/test_information_math.cpp
@@ -4,14 +4,12 @@
 using namespace std;
 
 TEST_CASE("information_math computation sanity checks") {
-    pair<int, int> whole(1, 2);
-    pair<int, int> left(1, 1);
-    pair<int, int> right(0, 1);
+    BinarySamples whole = {1, 2}, left = {1, 1}, right = {0, 1};
 
     REQUIRE(impurity(right) == 0);
     REQUIRE(impurity(left) == (double)1/8);
     REQUIRE(impurity(whole) < impurity(left));
-    REQUIRE(informationGain(left, right) == impurity(whole) - (double)2/3 * impurity(left) - (double)1/3 * impurity(right));
+    REQUIRE(jointImpurity(left, right) <= impurity(whole) * (whole.num_zeros + whole.num_ones));
 }
 
 //TODO test edge cases involving trivial splits, 0s, etc
