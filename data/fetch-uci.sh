59f84cfd05af58f622087be36c5d49625fd70b31
Samuel Drews <sedrews@wisc.edu>
2019-11-06 12:43:19 -0600

    fetch UCI datasets

---
+++ data/fetch-uci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+URLBASE=archive.ics.uci.edu/ml/machine-learning-databases/
+
+declare -A MAPBASETOFILES
+MAPBASETOFILES["iris"]="iris.names bezdekIris.data"
+MAPBASETOFILES["breast-cancer-wisconsin"]="wdbc.data wdbc.names"
+MAPBASETOFILES["wine"]="wine.data wine.names"
+
+URLS=($(
+for KEY in ${!MAPBASETOFILES[@]}; do
+    for FILE in ${MAPBASETOFILES[$KEY]}; do
+        echo ${URLBASE}${KEY}/${FILE};
+    done;
+done;))
+
+cd $(dirname "$0") # Descend into the same directory as the script
+wget -N ${URLS[@]}
