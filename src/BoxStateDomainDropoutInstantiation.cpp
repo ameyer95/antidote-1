06859d5156f3ebde6a88ba07afa2451bbc28f70c
Samuel Drews <sedrews@wisc.edu>
2019-11-08 15:39:06 -0600

    abstract transformers for numeric features

--- src/BoxStateDomainDropoutInstantiation.cpp
+++ src/BoxStateDomainDropoutInstantiation.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <list>
 #include <numeric> // for std::accumulate
+#include <set>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -12,6 +13,23 @@
 /**
  * TrainingReferencesWithDropout members
  */
+
+// first an auxiliary method
+void storeSymbolicPredicatesForFeatureValues(std::list<SymbolicPredicate> &ret, int index, std::vector<float> &feature_values) {
+    // XXX largely copy paste from concrete case
+    if(feature_values.size() == 0) {
+        return;
+    }
+
+    // std::set sorts, so we can use it to deduplicate + sort
+    std::set<float> unique_values(feature_values.begin(), feature_values.end());
+    feature_values.assign(unique_values.begin(), unique_values.end());
+
+    // For each adjacent pair (l,u) store a symbolic predicate x<=[l,u)
+    for(auto i = feature_values.cbegin(); i + 1 != feature_values.cend(); i++) {
+        ret.push_back(SymbolicPredicate(index, *i, *(i+1)));
+    }
+}
 
 TrainingReferencesWithDropout::TrainingReferencesWithDropout(DataReferences training_references, int num_dropout) {
     this->training_references = training_references;
@@ -36,7 +54,11 @@ std::list<SymbolicPredicate> TrainingReferencesWithDropout::gatherPredicates() c
                 ret.push_back(SymbolicPredicate(i));
                 break;
             case FeatureType::NUMERIC:
-                // XXX TODO need to implement numeric case
+                std::vector<float> feature_values(training_references.size());
+                for(unsigned int j = 0; j < training_references.size(); j++) {
+                    feature_values[j] = training_references[j].x[i].getNumericValue();
+                }
+                storeSymbolicPredicatesForFeatureValues(ret, i, feature_values);
                 break;
         }
     }
@@ -51,11 +73,15 @@ std::pair<TrainingReferencesWithDropout::DropoutCounts, TrainingReferencesWithDr
     for(unsigned int i = 0; i < training_references.size(); i++) {
         std::optional<bool> result = phi.evaluate(training_references[i].x);
         int category = training_references[i].y;
-        // XXX TODO strong assumption that result.has_value() --- need to be more general for numeric case
+        // XXX it should be an invariant of the way predicates are selected
+        // that we always have result.has_value()
+        // but this could be done more safely
         d_ptr = result.value() ? &(ret.second) : &(ret.first);
         d_ptr->counts[category]++;
     }
     // Ensure num_dropouts are well-defined XXX this is more complicated in the 3-valued case
+    // except that it's not---again, there should be an invariant that we never fall into
+    // maybe cases in this portion of the code
     std::vector<DropoutCounts*> iters = {&(ret.first), &(ret.second)};
     for(auto i = iters.begin(); i != iters.end(); i++) {
         (*i)->num_dropout = std::min(num_dropout, std::accumulate((*i)->counts.cbegin(), (*i)->counts.cend(), 0));
@@ -82,19 +108,21 @@ TrainingReferencesWithDropout TrainingReferencesWithDropout::pureSetRestriction(
 
 TrainingReferencesWithDropout TrainingReferencesWithDropout::filter(const SymbolicPredicate &phi, bool positive_flag) const {
     TrainingReferencesWithDropout ret(*this);
-    bool remove, result;
+    bool remove;
+    std::optional<bool> result;
+    int num_maybes = 0;
     for(unsigned int i = 0; i < ret.training_references.size(); i++) {
-        // XXX TODO we're not yet handling numeric cases
-        result = phi.evaluate(ret.training_references[i].x).value();
-        remove = (positive_flag != result);
+        result = phi.evaluate(ret.training_references[i].x);
+        if(!result.has_value()) {
+            num_maybes++;
+        }
+        remove = result.has_value() && (positive_flag != result.value());
         if(remove) {
             ret.training_references.remove(i);
             i--;
         }
     }
-    if(ret.num_dropout > ret.training_references.size()) {
-        ret.num_dropout = ret.training_references.size();
-    }
+    ret.num_dropout = std::min(ret.num_dropout + num_maybes, (int)ret.training_references.size());
     return ret;
 }
 
