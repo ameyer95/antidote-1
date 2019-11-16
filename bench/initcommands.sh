433bb0b8e8a7422638f0d62a4ca7da12f564f91b
Samuel Drews <sedrews@wisc.edu>
2019-11-16 13:51:35 -0600

    mnist experiments

--- bench/initcommands.sh
+++ bench/initcommands.sh
@@ -23,12 +23,34 @@ do
 done
 
 mkdir cancer
-for D in {1..6}
+for D in {1..4}
 do
     mkdir cancer/d${D}_a cancer/d${D}_V
     for I in {0..112}
     do
         echo ../bin/main -f ../data cancer -d $D -t $I -a 0 >> cancer/d${D}_a/initcommands_cancer_d${D}_a.txt
         echo ../bin/main -f ../data cancer -d $D -t $I -V 0 >> cancer/d${D}_V/initcommands_cancer_d${D}_V.txt
+    done
+done
+
+mkdir mnist_simple_1_7
+for D in {1..4}
+do
+    mkdir mnist_simple_1_7/d${D}_a mnist_simple_1_7/d${D}_V
+    for I in $(python3 test_mnist.py)
+    do
+        echo ../bin/main -f ../data mnist_simple_1_7 -d $D -t $I -a 0 >> mnist_simple_1_7/d${D}_a/initcommands_mnist_simple_1_7_d${D}_a.txt
+        echo ../bin/main -f ../data mnist_simple_1_7 -d $D -t $I -V 0 >> mnist_simple_1_7/d${D}_V/initcommands_mnist_simple_1_7_d${D}_V.txt
+    done
+done
+
+mkdir mnist_1_7
+for D in {1..4}
+do
+    mkdir mnist_1_7/d${D}_a mnist_1_7/d${D}_V
+    for I in $(python3 test_mnist.py)
+    do
+        echo ../bin/main -f ../data mnist_1_7 -d $D -t $I -a 0 >> mnist_1_7/d${D}_a/initcommands_mnist_1_7_d${D}_a.txt
+        echo ../bin/main -f ../data mnist_1_7 -d $D -t $I -V 0 >> mnist_1_7/d${D}_V/initcommands_mnist_1_7_d${D}_V.txt
     done
 done
