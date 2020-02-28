5e4ea0d2ee7c1cdf48bb7659a3cafce1fa3df383
Samuel Drews <sedrews@wisc.edu>
2020-02-28 13:05:52 -0600

    including a watered-down set of benchmarks

---
+++ scripts/batch-exp/run_all.sh
@@ -0,0 +1,44 @@
+# Two arguments: MEMLIMIT and TIMELIMIT, matching experiment.sh
+MEMLIMIT=$1
+TIMELIMIT=$2
+cd $(dirname "$0") # Descend into the same directory as this script
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d1_a/initcommands_iris_d1_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d2_a/initcommands_iris_d2_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d3_a/initcommands_iris_d3_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d4_a/initcommands_iris_d4_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d1_V/initcommands_iris_d1_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d2_V/initcommands_iris_d2_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d3_V/initcommands_iris_d3_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/iris/d4_V/initcommands_iris_d4_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d1_a/initcommands_mammography_d1_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d2_a/initcommands_mammography_d2_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d3_a/initcommands_mammography_d3_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d4_a/initcommands_mammography_d4_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d1_V/initcommands_mammography_d1_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d2_V/initcommands_mammography_d2_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d3_V/initcommands_mammography_d3_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mammography/d4_V/initcommands_mammography_d4_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d1_a/initcommands_cancer_d1_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d2_a/initcommands_cancer_d2_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d3_a/initcommands_cancer_d3_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d4_a/initcommands_cancer_d4_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d1_V/initcommands_cancer_d1_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d2_V/initcommands_cancer_d2_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d3_V/initcommands_cancer_d3_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/cancer/d4_V/initcommands_cancer_d4_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d1_a/initcommands_mnist_simple_1_7_d1_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d2_a/initcommands_mnist_simple_1_7_d2_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d3_a/initcommands_mnist_simple_1_7_d3_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d4_a/initcommands_mnist_simple_1_7_d4_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d1_V/initcommands_mnist_simple_1_7_d1_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d2_V/initcommands_mnist_simple_1_7_d2_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d3_V/initcommands_mnist_simple_1_7_d3_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_simple_1_7/d4_V/initcommands_mnist_simple_1_7_d4_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d1_a/initcommands_mnist_1_7_d1_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d2_a/initcommands_mnist_1_7_d2_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d3_a/initcommands_mnist_1_7_d3_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d4_a/initcommands_mnist_1_7_d4_a.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d1_V/initcommands_mnist_1_7_d1_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d2_V/initcommands_mnist_1_7_d2_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d3_V/initcommands_mnist_1_7_d3_V.txt
+./experiment.sh $MEMLIMIT $TIMELIMIT ../../bench/mnist_1_7/d4_V/initcommands_mnist_1_7_d4_V.txt
